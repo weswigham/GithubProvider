71b5e89ab7c7b9ba23a4166ff6c1910a567c32c9
Wesley Wigham <wwigham@gmail.com>
2015-05-12 21:40:42 -0400

    Move some classes into other files

---
+++ GithubProvider/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GithubProvider
+{
+    internal static class Extensions
+    {
+        public static T Resolve<T>(this Task<T> task)
+        {
+            return task.GetAwaiter().GetResult();
+        }
+    }
+}
