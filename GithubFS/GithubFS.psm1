221762635f2d4f37e6f15d3b90a6d0ca76ffa457
Wesley Wigham <wwigham@gmail.com>
2016-04-17 22:25:40 -0400

    case?

--- GithubFS/GithubFS.psm1
+++ GithubFS/GithubFS.psm1
@@ -44,7 +44,7 @@ function Out-Github {
 
 		try {
         		$full = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath($FilePath)
-        		$driveRoots = Get-PSProvider GitHub | Select-Object -ExpandProperty drives | foreach-object { $_.root }
+        		$driveRoots = Get-PSProvider Github | Select-Object -ExpandProperty drives | foreach-object { $_.root }
         		foreach ($root in $driveRoots) {
             			if ($full.StartsWith($root)) {
                 			$github = true
